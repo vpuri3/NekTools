c683a34a3a9ce59c078a41d84eb83cf04e0181ec
Vedant Puri <vpuri@vestalac2.ftd.alcf.anl.gov>
2019-02-07 17:41:15 -0600

    checks

--- budgets/load-verify/smoothWavyWall.f
+++ budgets/load-verify/smoothWavyWall.f
@@ -310,3 +310,18 @@ c-----------------------------------------------------------------------
       return
       END
 c-----------------------------------------------------------------------
+
+c automatically added by makenek
+      subroutine usrsetvert(glo_num,nel,nx,ny,nz) ! to modify glo_num
+      integer*8 glo_num(1)
+
+      return
+      end
+
+c automatically added by makenek
+      subroutine userqtl
+
+      call userqtl_scig
+
+      return
+      end
