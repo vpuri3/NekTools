9850d106809fdc7e5b4aab5e2a08a17ad268be04
Vedant Puri <vpuri3@illinois.edu>
2020-04-19 13:28:08 -0500

    verified wshr on theta

--- wshr/smoothWavyWall.f
+++ wshr/smoothWavyWall.f
@@ -309,3 +309,18 @@ c-----------------------------------------------------------------------
       return
       end
 C=======================================================================
+
+c automatically added by makenek
+      subroutine usrsetvert(glo_num,nel,nx,ny,nz) ! to modify glo_num
+      integer*8 glo_num(1)
+
+      return
+      end
+
+c automatically added by makenek
+      subroutine userqtl
+
+      call userqtl_scig
+
+      return
+      end
